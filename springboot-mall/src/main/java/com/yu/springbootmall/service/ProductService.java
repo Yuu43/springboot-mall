d8bddb251f0bf60f67f8377d66f171d2744a6ff5
Yu <10945085@ntub.edu.tw>
2025-05-23 23:20:59 +0800

    商品功能 - 實作查詢商品列表

--- springboot-mall/src/main/java/com/yu/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/yu/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.yu.springbootmall.service;
 import com.yu.springbootmall.dto.ProductRequest;
 import com.yu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+        List<Product> getProducts();
 
         Product getProductById(Integer productId);
 
